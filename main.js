a8b9e52e50f9081dc252eb66bae019a3c20e76be
noanthro <ailurophobisch@gmail.com>
2019-07-19 21:34:02 +0200

    cookie browser to request.jar function created

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 const fs = require('fs')
 const request = require('request')
-const cookie = require('tough-cookie-filestore')
-const uri = 'https://www.amazon.de/'
+const toughCookie = require('tough-cookie')
+const toughCookieFile = require('tough-cookie-filestore')
+
+const uri = 'https://streamlabs.com/dashboard#/stats'
+//const uri = 'https://www.amazon.de/'
 //const uri = 'https://streamlabs.com/api/v5/cloudbot/loyalty-store'
-let jar = request.jar(new cookie('./cookies.json'))
+let jar = getToughJar('./cookies.json')
+console.log(jar)
 request({
 		uri,
 		jar,
 	},
 	function (err,res,body){
-		console.log(res)
+		console.log(body)
 })
 
 
 function getToughJar(jsonPath) {
-	let importObj = JSON.parse(fs.readFileSync(jsonPath,{encoding:'utf8'}))
-	let jar = {}
-	importObj.forEach((cookieObj)=>{
-		
+	let testArr = []
+	let importArr = JSON.parse(fs.readFileSync(jsonPath,{encoding:'utf8'}))
+	let jar = request.jar() 
+	importArr.forEach(cookieObj => { 
+		let cookieString = ''
+		cookieString += cookieObj.name + '=' + cookieObj.value + ';'
+		for ( directive in cookieObj ) {
+			if (directive != 'name' || directive != 'value') {
+				cookieString += directive + '=' + cookieObj[directive] + ';'
+			}
+		}
+//		console.log(cookieString)
+		jar.setCookie(toughCookie.parse(cookieString), 'https://' + cookieObj.domain) //(cookieObj.domain.match(/[\w]+\.[\w]+$/))[0])
 	})	
-
+	return jar 
 } 
+
+
+//console.log(getToughJar("./browsercookies.json"))
